3c14194530718a80d582fa386e11745af79f0b8b
Daniel Kolsoi <thadan64@gmail.com>
2019-04-15 23:02:21 -0400

    Make tests pathing relative to Cargo manifest

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,6 +1,7 @@
 use cargo_tarpaulin::config::Config;
 use cargo_tarpaulin::launch_tarpaulin;
 use std::env;
+use std::path::PathBuf;
 use std::time::Duration;
 
 pub fn check_percentage(project_name: &str, minimum_coverage: f64, has_lines: bool) {
@@ -8,7 +9,7 @@ pub fn check_percentage(project_name: &str, minimum_coverage: f64, has_lines: bo
     config.verbose = true;
     config.test_timeout = Duration::from_secs(60);
     let restore_dir = env::current_dir().unwrap();
-    let mut test_dir = env::current_dir().unwrap();
+    let mut test_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     test_dir.push("tests");
     test_dir.push("data");
     test_dir.push(project_name);
@@ -36,7 +37,7 @@ fn incorrect_manifest_path() {
 fn proc_macro_link() {
     let mut config = Config::default();
     config.test_timeout = Duration::from_secs(60);
-    let mut test_dir = env::current_dir().unwrap();
+    let mut test_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     test_dir.push("tests");
     test_dir.push("data");
     test_dir.push("proc_macro");
